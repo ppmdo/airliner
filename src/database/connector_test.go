33a7edcaeae7897e2fa5e31c5b43aa96d4eedaf1
Pablo Prieto <pabloprieto@live.com>
2023-02-14 15:13:30 +0100

    added database package

---
+++ src/database/connector_test.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+    "context"
+    "testing"
+
+    "github.com/influxdata/influxdb-client-go/v2/domain"
+    "github.com/joho/godotenv"
+)
+
+func Test_connectToInfluxDB(t *testing.T) {
+
+    //load environment variable from a file for test purposes
+    godotenv.Load("../../test_influxdb.env")
+
+    tests := []struct {
+        name    string
+        wantErr bool
+    }{
+        {
+            name:    "Successful connection to InfluxDB",
+            wantErr: false,
+            },
+            }
+            for _, tt := range tests {
+                t.Run(tt.name, func(t *testing.T) {
+                    got, err := ConnectToInfluxDB()
+                    if (err != nil) != tt.wantErr {
+                        t.Errorf("ConnectToInfluxDB() error = %v, wantErr %v", err, tt.wantErr)
+                        return
+                    }
+                    health, err := got.Health(context.Background())
+                    if (err != nil) && health.Status == domain.HealthCheckStatusPass {
+                        t.Errorf("connectToInfluxDB() error. database not healthy")
+                        return
+                    }
+                    got.Close()
+                })
+            }
+}
---
+++ src/database/engine_test.go
@@ -0,0 +1,58 @@
+package database
+
+import (
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func Test_write_event_with_fluent_Style(t *testing.T) {
+	tests := []struct {
+		name  string
+		f     func(influxdb2.Client, []AirlineOffer)
+		datas []AirlineOffer
+	}{
+		{
+			name: "Write new record with line protocol",
+			// Your data Points
+			datas: mockData,
+			f: func(c influxdb2.Client, datas []AirlineOffer) {
+				// Send all the data to the DB
+				for _, data := range datas {
+					Write_event_with_fluent_Style(c, data)
+				}
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// helper to initialise and clean the database
+			client := init_testDB(t)
+			// call function under test
+			tt.f(client, tt.datas)
+			// TODO Validate the data
+			// test can be flicky if the query is done before that data is ready in the database
+			time.Sleep(time.Millisecond * 1000)
+
+			// Option one: QueryTableResult
+			results := read_events_as_query_table_result(client)
+			// convert results to array to compare with data
+			resultsArr := []AirlineOffer{}
+			for _, v := range results {
+				resultsArr = append(resultsArr, v)
+			}
+
+			if eq := reflect.DeepEqual(resultsArr, tt.datas); !eq {
+				t.Errorf("want %v, got %v", tt.datas, resultsArr)
+			}
+
+			// Option two: query raw data
+			// TODO add validation
+			read_events_as_raw_string(client)
+
+			client.Close()
+		})
+	}
+
+}
